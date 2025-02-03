0997103df964805bab20d0468bff3bc555aca9da
Mandisa2526 <92437885+Mandisa2526@users.noreply.github.com>
2025-02-03 14:36:23 +0200

    Delete src directory

--- src/interface/ITaxiQueue.tsx
+++
@@ -1,9 +0,0 @@
-export interface ITaxiQueue {
-    joinQueue(): string | null;
-    leaveQueue(): string | null;
-    joinTaxiQueue(): string | null;
-    taxiDepart(): string | null;
-    queueLength(): number;
-    taxiQueueLength(): number;
-}
-  
